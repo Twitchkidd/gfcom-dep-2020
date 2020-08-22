24f1f16f8fb8d10db574906e7853954f1befad09
Gareth Field <field.gareth@gmail.com>
2020-08-22 16:12:40 -0400

    started on index

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,50 @@
-import styled from 'styled-components';
-
-// const Title = styled.h1`
-// 	font-size: 50px;
-// 	color: ${({ theme }) => theme.colors.primary};
-// `;
-
-// export default function Home() {
-// 	return <Title>My page</Title>;
-// }
-
+import { useState } from 'react';
 import Head from 'next/head';
-import { Container, MoreStories, HeroPost, Intro, Layout } from '../components';
+import styled from 'styled-components';
+import { MoreStories, HeroPost, Layout } from '../components';
+import { above } from '../utils';
 import { getAllPosts } from '../lib';
+
+const Grid = styled.div`
+	width: 100%;
+	height: 100%;
+	display: grid;
+	grid-template-rows: fermf;
+	grid-template-columns: fermf;
+	${above.small`
+		grid-template-rows: fermf;
+		grid-template-columns: fermf;
+	`}
+`;
+
+const Sidebar = styled.div``;
+
+const Header1 = styled.header``;
+
+const Nav = styled.nav``;
 
 export default function Index({ allPosts }) {
 	const heroPost = allPosts[0];
 	const morePosts = allPosts.slice(1);
+	const [index, setIndex] = useState(1);
 	return (
 		<>
 			<Layout>
 				<Head>
 					<title>Gareth Field ... dot com!</title>
 				</Head>
-				<Container>
-					<Intro />
-					{heroPost && (
+				<Grid>
+					<Tabs state={[index, setIndex]}>
+						<Sidebar>
+							<Header1 />
+							<Nav>
+								<Tabs />
+								<Links />
+								<Socials />
+							</Nav>
+						</Sidebar>
+						<Header2 />
+						<Header3 />
 						<HeroPost
 							title={heroPost.title}
 							// coverImage={heroPost.coverImage}
@@ -33,9 +53,9 @@ export default function Index({ allPosts }) {
 							slug={heroPost.slug}
 							excerpt={heroPost.excerpt}
 						/>
-					)}
-					{morePosts.length > 0 && <MoreStories posts={morePosts} />}
-				</Container>
+						{morePosts.length > 0 && <MoreStories posts={morePosts} />}
+					</Tabs>
+				</Grid>
 			</Layout>
 		</>
 	);
@@ -55,3 +75,8 @@ export async function getStaticProps() {
 		props: { allPosts },
 	};
 }
+
+// const Title = styled.h1`
+// 	font-size: 50px;
+// 	color: ${({ theme }) => theme.colors.primary};
+// `;
