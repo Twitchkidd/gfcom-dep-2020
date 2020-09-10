02a10e746d41b1d538b2f427e51188b6e8e5ed97
Gareth Field <field.gareth@gmail.com>
2020-09-10 14:00:30 -0400

    no cells!

--- pages/virtual-garage-sale.js
+++ pages/virtual-garage-sale.js
@@ -1,12 +1,19 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+// import addDays from 'date-fns/addDays';
 import Link from 'next/link';
 import Carousel from '@brainhubeu/react-carousel';
 import { Line } from 'rc-progress';
 import { Layout } from '../components';
-import { elevation, veryLight, darkPink, darkPurple, darkBlue } from '../utils';
+import {
+	elevation,
+	veryLight,
+	darkPink,
+	darkPurple,
+	darkBlue,
+	slugify,
+} from '../utils';
 import { getAllItems } from '../lib';
-
-const CarouselHeaderWrap = styled.header``;
 
 const Header = styled.h1``;
 
@@ -28,133 +35,219 @@ const CategoryWrap = styled.section``;
 
 const CategoryHeader = styled.h2``;
 
-const CardWrap = styled.div``;
+const CardWrap = styled.a`
+	display: block;
+	border: 2px solid ${props => (props.sold ? 'green' : 'gray')};
+	border-radius: 0.5rem;
+	${elevation[0]}
+`;
+
+const DetailsWrap = styled.div`
+	max-width: 300px;
+	min-width: 150px;
+`;
+
+const ImageWrap = styled.div`
+	position: relative;
+	max-width: 100%;
+	float: right;
+`;
+
+const Image = styled.img`
+	max-width: 200px;
+`;
+
+const PriceTag = styled.div`
+	position: absolute;
+	top: -0.5rem;
+	left: -0.5rem;
+	border-radius: 0.5rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Price = styled.span`
+	font-size: 0.75rem;
+`;
+
+const PreviousPrice = styled(Price)`
+	color: red;
+	text-decoration: line-through;
+`;
 
 const Card = ({ item }) => {
-	const { description, name, soldOn } = item;
+	const {
+		soldOn,
+		startDate,
+		title,
+		description,
+		image,
+		link,
+		platform,
+		auctionPrice,
+		price,
+		previousPrice,
+	} = item;
 	return (
-		<CardWrap homed={soldOn}>
-			<h3>{name}</h3>
-			<p>{description}</p>
-			{/* <img? || CARUSEL?! */}
+		<CardWrap
+			sold={soldOn !== ''}
+			href={link}
+			alt={platform + ' post'}
+			platform={platform}>
+			<DetailsWrap>
+				<h3>{title}</h3>
+				<p>{description}</p>
+				<p>Click/tap to go to {platform}.com!</p>
+			</DetailsWrap>
+			<ImageWrap>
+				<Image src={require('../public/fortTrumbull.jpg')} />
+				<PriceTag>
+					{auctionPrice === '' ? (
+						previousPrice === '' ? (
+							<Price>${price}</Price>
+						) : (
+							<>
+								<PreviousPrice>${previousPrice}</PreviousPrice>
+								<Price> {price}</Price>
+							</>
+						)
+					) : previousPrice === '' ? (
+						<Price>
+							${auctionPrice}/${price} Auction/Buy Now
+						</Price>
+					) : (
+						<>
+							<PreviousPrice>${previousPrice}</PreviousPrice>
+							<Price>
+								{' '}
+								${auctionPrice}/${price} Auction/Buy Now
+							</Price>
+						</>
+					)}
+				</PriceTag>
+			</ImageWrap>
+			{/* TODO {platform === 'Craigslist' ? null : <p>{startDate}</p>} */}
 		</CardWrap>
 	);
 };
 
-const blurb = 'Four score and seven years ago ...';
+const blurb = "Let's find homes for everything Gareth needs to sell!";
 
 export default function VirtualGarageSale({ rows }) {
-	const handledRows = rows.map(row => {
-		return {
-			name: row[18] !== '' ? row[19] : row[12] !== '' ? row[13] : row[6],
-			sold: row[3] !== '',
-		};
-	});
-	console.log(handledRows);
-	const categoriesArray = Array.from(new Set(rows.map(row => row[1]).sort()));
-	const categories = categoriesArray.map(category => ({
-		name: category,
-		items: handledRows.filter(item => item.category === category),
-	}));
+	const [categories, setCategories] = useState(null);
+	const [totalPercent, setTotalPercent] = useState();
+	useEffect(() => {
+		const categoriesArray = Array.from(new Set(rows.map(row => row[2]).sort()));
+		const handledItems = rows.map(row => {
+			const attemptNumber = row[19] !== '' ? 3 : row[12] !== '' ? 2 : 1;
+			return {
+				category: row[2],
+				soldOn: row[3],
+				startDate: [row[5], row[12], row[18]][attemptNumber - 1],
+				title: [row[6], row[13], row[19]][attemptNumber - 1],
+				description: [row[7], row[14], row[20]][attemptNumber - 1],
+				// image: [row[8], row[15], row[21]][attemptNumber - 1].includes(',')
+				// 	? [row[8], row[15], row[21]][attemptNumber - 1].split(',')[0].trim()
+				// 	: [row[8], row[15], row[21]][attemptNumber - 1],
+				image: 'fermf',
+				link: [row[9], row[16], row[22]][attemptNumber - 1],
+				// platform: [row[9], row[16], row[22]][attemptNumber - 1].includes('ebay')
+				// 	? 'eBay'
+				// 	: 'Craigslist',
+				platform: 'Craigslist',
+				auctionPrice: [row[10], row[17], row[23]][attemptNumber - 1],
+				price: [row[11], row[18], row[24]][attemptNumber - 1],
+				previousPrice:
+					attemptNumber > 2 ? row[18] : attemptNumber > 1 ? row[11] : null,
+			};
+		});
+		setCategories(
+			categoriesArray.map(category => ({
+				name: category,
+				items: handledItems.filter(item => item.category === category),
+				percent:
+					Math.round(
+						handledItems
+							.filter(item => item.category === category)
+							.filter(item => item.soldOn !== '').length /
+							handledItems.filter(item => item.category === category).length
+					) * 100,
+			}))
+		);
+	}, []);
+	useEffect(() => {
+		if (categories) {
+			const items = categories.map(category => category.items).flat();
+			setTotalPercent(
+				Math.round(
+					items.filter(item => item.soldOn !== '').length / items.length
+				) * 100
+			);
+		}
+	}, [categories]);
 	let flights = [];
 	flights.length = 4;
 	return (
 		<Layout>
-			Hey
-			{/* <CarouselHeaderWrap>
-				<Header>Virtual Garage Sale</Header>
-				<Carousel>
-					{flights.map((flight, i) => (
-						<Slide
-							key={`Flight number ${i}.`}
-							src={require(`../public/virtualGarageSale/flight${i + 1}.jpg`)}
-							alt={`Flights of items, batch ${i + 1}.`}
+			{!categories ? null : (
+				<>
+					<Header>Virtual Garage Sale</Header>
+					<Blurb>{blurb}</Blurb>
+					<Carousel>
+						{flights.map((f, i) => (
+							<Slide
+								key={`Flight number ${i}.`}
+								src={require(`../public/virtualGarageSale/flight${i + 1}.jpg`)}
+								alt={`Flights of items, batch ${i + 1}.`}
+							/>
+						))}
+					</Carousel>
+					<ProgressWrap>
+						<Line
+							percent={totalPercent}
+							strokeWidth='6'
+							strokeColor={[
+								{
+									'100%': `${darkBlue}`,
+									'50%': `${darkPurple}`,
+									'0%': `${darkPink}`,
+								},
+								veryLight,
+							]}
 						/>
+						{/* green? */}
+					</ProgressWrap>
+					<TableOfCategoriesWrap>
+						{categories.map(category => (
+							<CategoryButton key={category.name}>
+								<Link href={`/virtual-garage-sale#${slugify(category.name)}`}>
+									<CategoryButtonA>{category.name}</CategoryButtonA>
+								</Link>
+							</CategoryButton>
+						))}
+					</TableOfCategoriesWrap>
+					{categories.map(category => (
+						<CategoryWrap key={category.name} id={slugify(category.name)}>
+							<CategoryHeader>{category.name}</CategoryHeader>
+							{category.items.map(item => (
+								<Card key={item.name} item={item} />
+							))}
+						</CategoryWrap>
 					))}
-				</Carousel>
-			</CarouselHeaderWrap>
-			<Blurb>{blurb}</Blurb>
-			<ProgressWrap>
-				<Line
-					percent={[percent, 100 - percent]}
-					strokeWidth='6'
-					strokeColor={[
-						{
-							'100%': `${darkBlue}`,
-							'50%': `${darkPurple}`,
-							'0%': `${darkPink}`,
-						},
-						veryLight,
-					]}
-				/>
-			</ProgressWrap>
-			<TableOfCategoriesWrap>
-				{categories.map(category => (
-					<CategoryButton key={category.name}>
-						<Link href={`/virtual-garage-sale#${category.slug}`}>
-							<CategoryButtonA>{category.name}</CategoryButtonA>
-						</Link>
-					</CategoryButton>
-				))}
-			</TableOfCategoriesWrap>
-			{categories.map(category => (
-				<CategoryWrap key={category.name}>
-					<CategoryHeader>{category.name}</CategoryHeader>
-					{category.items.map(item => (
-						<Card key={item.name} item={item} />
-					))}
-				</CategoryWrap>
-			))} */}
+				</>
+			)}
 		</Layout>
 	);
 }
 
 export async function getStaticProps() {
 	const rows = await getAllItems();
-	/*
-		- Percent complete
-		- Categories ✅
-
-		- Items: (BY CATEGORY!)
-			- Not Sold:
-				- Most recent:
-					- Title
-					- Description
-					- Image (out of images)
-					- Link
-					- Platform
-						- Price or Auction/Buy price ... oooh, how do you look up the current auction rather than the start?
-						- Any previous price/auction prices
-		- Sold?
-			- Title
-			- Description
-			- Image (out of images)
-	*/
+	console.log(rows);
 	return {
 		props: { rows },
 	};
 }
-
-/*
-
-flights = [../public/vgs/f1, ../public/vgs/f2, ...]
-
-total = items.length
-sold = soldItems.length
-
-categories = items.map().filter()
-
-items = items.map(item =>
-	if items.soldFor {
-		{ title, description, category, images, soldOn } = item
-	} else if (items.saleAttempt[saleAttempts.length]) {
-		{ title, description, category, images, link, saleAttemptAuctionPrice, saleAttemptPrice } = item
-	} else {
-		{ title, description, category, images, link, saleAttemptPrice } = item
-	}
-)
-
-*/
 
 // const ItemsGrid = styled.div`
 // 	width: 90%;
--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,7 @@ import elevation from './Elevation';
 export * from './Breakpoints';
 export * from './Colors';
 export * from './Quotes';
+export * from './slugify';
 export * from './Socials';
 export * from './storageAvailable';
 
