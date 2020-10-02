5a3faf1d924e9782e2e871f31e483b35c761397a
Gareth Field <field.gareth@gmail.com>
2020-10-02 07:40:28 -0400

    added updated blurb

--- siteMetadata.js
+++ siteMetadata.js
@@ -1,7 +1,7 @@
 export const metadata = {
 	siteTitle: 'Gareth Field ... Dot Com!',
 	siteDescription:
-		'A homepage for all things, running-, coding-, coffee-, Dawa- (my dog 😃) and Gareth- related on the Internet!',
+		'A homepage for all things, running-, coding-, coffee-, Gareth-, and Dawa- (my dog 😃) related on the Internet!',
 	blurb:
-		'Gareth is a trailrunner, developer, and dog-dad based out of beautiful downtown New London, Connecticut!',
+		'Gareth is a trailrunner, developer, and dog-dad based out of beautiful downtown New London, Connecticut. Thanks for visiting!',
 };
